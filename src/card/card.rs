f5c8071b9a7313d1461b8cefb56ce8edbb181417
magus <noah@iamnoah.com>
2022-10-02 17:37:54 -0700

    card: print name only for now

--- src/card/card.rs
+++ src/card/card.rs
@@ -1,10 +1,18 @@
+use std::fmt;
+
 pub const EMPTY: Card = Card {
     name: "EMPTY",
     sides: (0, 0, 0, 0),
 };
 
-#[derive(Copy, Clone, PartialEq, Debug)]
+#[derive(Copy, Clone, PartialEq)]
 pub struct Card {
     pub name: &'static str,
     pub sides: (u8, u8, u8, u8),
+}
+
+impl fmt::Debug for Card {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.name)
+    }
 }
