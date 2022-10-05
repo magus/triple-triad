c7478ffd9efde7800d41fa88b0efc5820da19b87
magus <noah@iamnoah.com>
2022-10-04 23:35:16 -0700

    card: colorized colors and prints

--- src/card/card.rs
+++ src/card/card.rs
@@ -1,7 +1,7 @@
 use std::fmt;
 
 pub const EMPTY: Card = Card {
-    name: "__",
+    name: "  ",
     sides: (0, 0, 0, 0),
     is_player: false,
     is_empty: true,
@@ -53,10 +53,45 @@ impl Card {
     pub fn left(&self) -> u8 {
         return self.sides.3;
     }
+
+    fn print_side(&self, side: u8) -> String {
+        return if self.is_empty {
+            " ".to_string()
+        } else {
+            format!("{}", side)
+        };
+    }
+
+    pub fn print_top(&self) -> String {
+        self.print_side(self.sides.0)
+    }
+
+    pub fn print_right(&self) -> String {
+        self.print_side(self.sides.1)
+    }
+
+    pub fn print_bottom(&self) -> String {
+        self.print_side(self.sides.2)
+    }
+
+    pub fn print_left(&self) -> String {
+        self.print_side(self.sides.3)
+    }
+
+    pub fn rgb_color(&self) -> ((u8, u8, u8), (u8, u8, u8)) {
+        return if self.is_empty {
+            ((0, 0, 0), (140, 140, 140))
+        } else if self.is_player {
+            ((200, 200, 200), (65, 88, 120))
+        } else {
+            ((200, 200, 200), (95, 40, 50))
+        };
+    }
 }
 
 impl fmt::Debug for Card {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}", self.name)
+        let (top, right, bottom, left) = self.sides;
+        write!(f, "{}[{},{},{},{}]", self.name, top, right, bottom, left)
     }
 }
