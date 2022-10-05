81528324066cd0652c2585adf4de4043a298f873
magus <noah@iamnoah.com>
2022-10-04 23:42:02 -0700

    custom simulation with exact moves

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 use rayon::prelude::*;
+use triple_triad::card::Card;
 use triple_triad::game::{constants, Game};
 use triple_triad::time::Stopwatch;
 
 fn main() {
     // explore();
 
-    simple_simulation();
+    // simple_simulation();
+
+    custom_simulation();
 
     // parallel_explore_max();
 }
@@ -25,6 +28,57 @@ fn explore() {
 
     game.start_explore();
     stopwatch.record("explore finished");
+}
+
+#[allow(dead_code)]
+fn custom_simulation() {
+    let mut game = Game::new();
+
+    // game.rules.same = true;
+    // game.rules.plus = true;
+
+    game.player.cards = [
+        Card::player("P0", 1, 1, 1, 1),
+        Card::player("P1", 1, 1, 1, 1),
+        Card::player("P2", 1, 2, 1, 1),
+        Card::player("P3", 1, 1, 1, 1),
+        Card::player("P4", 1, 2, 3, 2),
+    ];
+
+    game.computer.cards = [
+        Card::computer("C0", 1, 1, 2, 1),
+        Card::computer("C1", 1, 1, 1, 3),
+        Card::computer("C2", 2, 1, 1, 1),
+        Card::computer("C3", 4, 1, 4, 1),
+        Card::computer("C4", 1, 1, 1, 1),
+    ];
+
+    game = game.execute_turn(0, 0);
+    println!("{:?}", game);
+
+    game = game.execute_turn(0, 1);
+    println!("{:?}", game);
+
+    game = game.execute_turn(1, 2);
+    println!("{:?}", game);
+
+    game = game.execute_turn(1, 5);
+    println!("{:?}", game);
+
+    game = game.execute_turn(2, 6);
+    println!("{:?}", game);
+
+    game = game.execute_turn(2, 7);
+    println!("{:?}", game);
+
+    game = game.execute_turn(3, 8);
+    println!("{:?}", game);
+
+    game = game.execute_turn(3, 3);
+    println!("{:?}", game);
+
+    game = game.execute_turn(4, 4);
+    println!("{:?}", game);
 }
 
 #[allow(dead_code)]
