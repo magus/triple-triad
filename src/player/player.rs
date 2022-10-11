67a5c9b50bc68c6397f588c71ad5a0dbb1578af4
magus <noah@iamnoah.com>
2022-10-11 01:23:43 -0700

    maybe_filter_hand_guaranteed

--- src/player/player.rs
+++ src/player/player.rs
@@ -32,18 +32,19 @@ impl Player {
     }
 }
 
-const COMPUTER_HAND_SIZE: usize = 7;
+const MAX_COMPUTER_HAND_SIZE: usize = 8;
 
 #[derive(Clone, Debug)]
 pub struct Computer {
-    pub cards: [Card; COMPUTER_HAND_SIZE],
+    pub cards: [Card; MAX_COMPUTER_HAND_SIZE],
+    pub cards_used: usize,
 }
 
 impl Computer {
     pub fn cards_left(&self) -> Vec<usize> {
         let mut card_index_list = vec![];
 
-        for i in 0..COMPUTER_HAND_SIZE {
+        for i in 0..MAX_COMPUTER_HAND_SIZE {
             if self.cards[i] != card::EMPTY {
                 card_index_list.push(i);
             }
@@ -57,8 +58,43 @@ impl Computer {
 
         if card != card::EMPTY {
             self.cards[index] = card::EMPTY;
+            self.cards_used += 1;
         }
 
         return card;
+    }
+
+    // computer can have between 6-8 cards it seems
+    // of those between 2-4 seem to be guaranteed
+    // we can use this fact to predict remaining cards
+    //
+    // examples
+    // https://arrtripletriad.com/en/npc-droyn
+    // https://arrtripletriad.com/en/npc-aiglephine
+    // https://arrtripletriad.com/en/npc-prudence
+    // https://arrtripletriad.com/en/npc-ghasa
+    pub fn maybe_filter_hand_guaranteed(&mut self) {
+        // count how many cards in hand are left
+        let hand_remaining = PLAYER_HAND_SIZE - self.cards_used;
+
+        // count how many guaranteed cards are remaining
+        let mut guaranteed_remaining = vec![];
+
+        for i in 0..MAX_COMPUTER_HAND_SIZE {
+            if self.cards[i].is_guaranteed {
+                guaranteed_remaining.push(i);
+            }
+        }
+
+        // if equal, remove non-guaranteed cards from hand
+        if hand_remaining == guaranteed_remaining.len() {
+            // println!("🚨 Guaranteed remaining cards!");
+
+            for i in 0..MAX_COMPUTER_HAND_SIZE {
+                if !self.cards[i].is_guaranteed {
+                    self.cards[i] = card::EMPTY;
+                }
+            }
+        }
     }
 }
