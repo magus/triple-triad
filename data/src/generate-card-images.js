45a6e67594b5b63d18c0cbbb52d37f7096cebe39
magus <noah@iamnoah.com>
2022-10-23 00:41:40 -0700

    data: super clean card image generation

--- data/src/generate-card-images.js
+++ data/src/generate-card-images.js
@@ -6,95 +6,187 @@ import Jimp from 'jimp';
 import { game_dir } from './constants.js';
 import * as json from './json.js';
 
-const cards = json.read(game_dir('cards.json'));
-// console.debug({ cards });
+const CARDS = json.read(game_dir('cards.json'));
+// console.debug({ CARDS });
 
-async function main() {
-  // start with background and composite on top of it
-  const image = await Jimp.read(game_dir('images', 'background-blue.png'));
+const IMAGES = {};
+IMAGES.Star = await Jimp.read(game_dir('images', 'star.png'));
+IMAGES.Background = {};
+IMAGES.Background.gray = await Jimp.read(game_dir('images', 'background-gray.png'));
+IMAGES.Background.blue = await Jimp.read(game_dir('images', 'background-blue.png'));
+IMAGES.Background.red = await Jimp.read(game_dir('images', 'background-red.png'));
 
+const { scale, width, height } = await (async function () {
   // read in first card to capture measurements
-  const card_id = '74';
-  const card_image = await Jimp.read(card_image_path(card_id));
-  image.composite(card_image, 0, 0);
+  const image = await create_card_base({ id: '1', background: 'gray' });
 
   // 208x256    hd card image
   // 200x252    actual card dimensions excluding padding
   // we can autocrop to actual card dimensions
   image.autocrop();
 
-  const star_size = 13;
-  const card_width = 90;
-  const card_height = 114;
+  const width = image.getWidth();
+  const height = image.getHeight();
 
-  // calculated by using reference photos from game
+  // constants below are calculated by using reference photos from game
   // see https://www.figma.com/file/O39AUh8Rbog2MQRykgrorO/FFXIV-Triple-Triad?node-id=0%3A1
-  const star_pos_list = [
-    [13, 4],
-    [5, 9],
-    [21, 9],
-    [9, 17],
-    [18, 17],
-  ];
+  // 90 is the width of the reference card in figma
+  const scalar = width / 90;
 
-  const num_pos_list = [
-    { side: 'top', pos: [37, 80] },
-    { side: 'right', pos: [48, 85] },
-    { side: 'bottom', pos: [37, 91] },
-    { side: 'left', pos: [26, 85] },
-  ];
+  function scale(value) {
+    return Math.round(value * scalar);
+  }
 
-  const scalar = image.getWidth() / card_width;
+  return { scale, width, height };
+})();
 
-  const star_image = await Jimp.read(game_dir('images', 'star.png'));
+IMAGES.Number = {};
+for (let i = 0; i < 10; i++) {
+  const num = String(i + 1);
 
-  const star_count = 5;
+  const image = await Jimp.read(number_image_path(num));
+
+  // the numbers are 15x15 in figma reference card, so we scale them
+  image.resize(scale(15), scale(15));
+
+  IMAGES.Number[num] = image;
+}
+
+IMAGES.Tribe = {};
+IMAGES.Tribe.garlean = await Jimp.read(game_dir('images', 'garlean.png'));
+IMAGES.Tribe.primal = await Jimp.read(game_dir('images', 'primal.png'));
+IMAGES.Tribe.scion = await Jimp.read(game_dir('images', 'scion.png'));
+IMAGES.Tribe.beastman = await Jimp.read(game_dir('images', 'beastman.png'));
+
+const POS = {};
+POS.StarList = [
+  [scale(13), scale(4)],
+  [scale(5), scale(9)],
+  [scale(21), scale(9)],
+  [scale(9), scale(17)],
+  [scale(18), scale(17)],
+];
+
+POS.NumberList = [
+  { side: 'top', pos: [scale(37), scale(80)] },
+  { side: 'right', pos: [scale(48), scale(85)] },
+  { side: 'bottom', pos: [scale(37), scale(91)] },
+  { side: 'left', pos: [scale(26), scale(85)] },
+];
+
+POS.Tribe = [scale(70), scale(1)];
+
+await main();
+
+async function main() {
+  const params = {
+    id: '74',
+    background: 'gray',
+    stars: 5,
+    top: 9,
+    right: 10,
+    bottom: 9,
+    left: 1,
+  };
+
+  const output = (function (columns, rows) {
+    let x = 0;
+    let y = 0;
+    const card_width = width + 16;
+    const card_height = height + 16;
+
+    const image = new Jimp(card_width * 5, card_height * 5, 'transparent');
+
+    function move_right() {
+      const result = [x, y];
+      x += card_width;
+      return result;
+    }
+
+    function move_down() {
+      const result = [x, y];
+      y += card_height;
+      x = 0;
+      return result;
+    }
+
+    function write(add_image) {
+      image.composite(add_image, ...move_right());
+    }
+
+    return { write, move_down, image };
+  })();
+
+  for (const background of Object.keys(IMAGES.Background)) {
+    output.write(await create_card({ ...params, background }));
+  }
+
+  output.move_down();
+
+  for (let i = 0; i < 5; i++) {
+    const stars = i + 1;
+
+    output.write(await create_card({ ...params, stars }));
+  }
+
+  output.move_down();
+
+  for (const tribe of Object.keys(IMAGES.Tribe)) {
+    output.write(await create_card({ ...params, tribe }));
+  }
+
+  output.move_down();
+
+  for (const number of Object.keys(IMAGES.Number)) {
+    const left = number;
+    output.write(await create_card({ ...params, left }));
+    if (number === '5') {
+      output.move_down();
+    }
+  }
+
+  output.image.write('output.png');
+}
+
+async function create_card(params) {
+  const image = await create_card_base(params);
+
+  const star_count = params.stars;
 
   // draw stars in reverse order so 4 and 5 are under 2 and 3
   for (let i = 0; i < star_count; i++) {
-    const star_pos = star_pos_list[star_count - i - 1];
-
-    const [x, y] = star_pos.map((n) => Math.round(n * scalar));
-
-    image.composite(star_image, x, y);
+    const [x, y] = POS.StarList[star_count - i - 1];
+    image.composite(IMAGES.Star, x, y);
   }
 
-  const card = {
-    top: 9,
-    right: 3,
-    bottom: 9,
-    left: 8,
-  };
+  const { top, right, bottom, left } = params;
+  const card = { top, right, bottom, left };
 
-  const scale_num_image = Math.round(15 * scalar);
-
-  const number_image_map = {};
-  for (let i = 0; i < 10; i++) {
-    const num = String(i + 1);
-    const image = await Jimp.read(number_image_path(num));
-    image.resize(scale_num_image, scale_num_image);
-    number_image_map[num] = image;
-  }
-
-  for (const num_pos of num_pos_list) {
-    const [x, y] = num_pos.pos.map((n) => Math.round(n * scalar));
-    const num_image = number_image_map[card[num_pos.side]];
+  for (const num_pos of POS.NumberList) {
+    const [x, y] = num_pos.pos;
+    const num_image = IMAGES.Number[card[num_pos.side]];
 
     image.composite(num_image, x, y);
   }
 
-  const tribe_image = await Jimp.read(game_dir('images', 'garlean.png'));
-  const [x, y] = [70, 1].map((n) => Math.round(n * scalar));
-  image.composite(tribe_image, x, y);
+  if (params.tribe) {
+    const [x, y] = POS.Tribe;
+    image.composite(IMAGES.Tribe[params.tribe], x, y);
+  }
 
-  image.write(`${card_id}.png`);
+  return image;
 }
 
-main();
+async function create_card_base(params) {
+  // start with clone of background and composite on top of it
+  const image = IMAGES.Background[params.background].clone();
 
-// let image = new Jimp(300, 530, "green", (err, image) => {
-//   if (err) throw err;
-// });
+  const card_id = params.id;
+  const card_image = await Jimp.read(card_image_path(card_id));
+  image.composite(card_image, 0, 0);
+
+  return image;
+}
 
 function card_image_path(card_id) {
   return game_image_path(82100, card_id);
