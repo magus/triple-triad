02b49e5c3f4e7ee550736e1d6b303cb34d5190ac
magus <noah@iamnoah.com>
2022-10-26 00:08:52 -0700

    ui: spread image static data

--- src/components/Tile/Tile.tsx
+++ src/components/Tile/Tile.tsx
@@ -3,5 +3,13 @@ import Image from 'next/image';
 import BoardSquare from './board-square.png';
 
 export function Tile() {
-  return <Image className="rotate-0 object-contain" src={BoardSquare.src} alt="tile" />;
+  return (
+    <Image
+      className="rotate-0 object-contain"
+      src={BoardSquare.src}
+      alt="tile"
+      width={BoardSquare.width}
+      height={BoardSquare.height}
+    />
+  );
 }
