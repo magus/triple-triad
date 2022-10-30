46c5278f64e315d8ad6603ec4d3bbcca1148a5eb
magus <noah@iamnoah.com>
2022-10-30 01:23:03 -0700

    ui: darken odd tiles

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -4,19 +4,19 @@ export function Board() {
   return (
     <div className="inline-block">
       <div className="flex flex-row">
-        <Tile />
-        <Tile />
-        <Tile />
+        <Tile id={1} />
+        <Tile id={2} />
+        <Tile id={3} />
       </div>
       <div className="flex flex-row">
-        <Tile />
-        <Tile />
-        <Tile />
+        <Tile id={4} />
+        <Tile id={5} />
+        <Tile id={6} />
       </div>
       <div className="flex flex-row">
-        <Tile />
-        <Tile />
-        <Tile />
+        <Tile id={7} />
+        <Tile id={8} />
+        <Tile id={9} />
       </div>
     </div>
   );
--- src/components/Tile/Tile.tsx
+++ src/components/Tile/Tile.tsx
@@ -2,15 +2,28 @@ import Image from 'next/image';
 
 import BoardSquare from './board-square.png';
 
-export function Tile() {
+type Props = {
+  id: number;
+};
+
+export function Tile(props: Props) {
+  const { width, height } = BoardSquare;
+  const dimensions = { width, height };
+
   return (
-    <Image
-      className="rotate-0"
-      src={BoardSquare.src}
-      layout="fixed"
-      alt="tile"
-      width={BoardSquare.width}
-      height={BoardSquare.height}
-    />
+    <div className="relative" style={dimensions}>
+      <Image className="rotate-0" src={BoardSquare.src} layout="fixed" alt="tile" {...dimensions} />
+      <MaybeDark {...props} />
+    </div>
   );
+}
+
+function MaybeDark(props: Props) {
+  const is_even = props.id % 2 === 0;
+
+  if (is_even) {
+    return null;
+  }
+
+  return <div className="absolute top-0 left-0 h-full w-full bg-stone-700 opacity-20" />;
 }
