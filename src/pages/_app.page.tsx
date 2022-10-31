56ded6aa4ea6df82347ff86a4b7006c0fd1bed88
magus <noah@iamnoah.com>
2022-10-30 18:34:34 -0700

    app: disable zoom which breaks drag animation

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -8,38 +8,40 @@ import './global.css';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   // setup global event listeners here
+  // global event listeners are active even when window isn't focused
+  // prefer normal keyboard listeners instead for window shortcuts
   // such as restart or quit
   // forcefully route to specific page, refresh state, etc.
-  const default_zoom = 0.6;
-  const [zoom, set_zoom] = React.useState(default_zoom);
-  const inc_zoom = (sign: -1 | 1) => (z: number) => +Math.max(0.4, Math.min(0.8, z + 0.1 * sign)).toFixed(2);
+
+  // const default_zoom = 1.0;
+  // const [zoom, set_zoom] = React.useState(default_zoom);
+  // const inc_zoom = (sign: -1 | 1) => (z: number) => +Math.max(0.4, Math.min(0.8, z + 0.1 * sign)).toFixed(2);
 
   React.useEffect(() => {
     if (!isTauriApp()) return;
 
-    const TauriWindow = require('@tauri-apps/api/window');
+    // const TauriWindow = require('@tauri-apps/api/window');
+    // console.debug(TauriWindow);
+    // window['TauriWindow'] = TauriWindow;
 
     // setup zoom level and window size in array
     // selection becomes index in array instead of raw zoom level
     // set window size for each zoom level to match scale
     // e.g. large scale, large window ... small scale, small window
-    console.debug(TauriWindow);
-    window['TauriWindow'] = TauriWindow;
+
     // TauriWindow.appWindow.setSize(new TauriWindow.LogicalSize(800, 600));
 
     async function run() {
       // https://tauri.app/v1/api/js/globalShortcut
-      await TauriGlobalShortcut.unregisterAll();
-      await TauriGlobalShortcut.unregister('CommandOrControl+M');
-      await TauriGlobalShortcut.unregister('CommandOrControl+0');
-      await TauriGlobalShortcut.unregister('CommandOrControl+-');
-      await TauriGlobalShortcut.unregister('CommandOrControl+=');
-
-      TauriGlobalShortcut.register('CommandOrControl+M', () => TauriWindow.appWindow.minimize());
-
-      TauriGlobalShortcut.register('CommandOrControl+0', () => set_zoom(default_zoom));
-      TauriGlobalShortcut.register('CommandOrControl+-', () => set_zoom(inc_zoom(-1)));
-      TauriGlobalShortcut.register('CommandOrControl+=', () => set_zoom(inc_zoom(+1)));
+      // await TauriGlobalShortcut.unregisterAll();
+      // await TauriGlobalShortcut.unregister('CommandOrControl+M');
+      // await TauriGlobalShortcut.unregister('CommandOrControl+0');
+      // await TauriGlobalShortcut.unregister('CommandOrControl+-');
+      // await TauriGlobalShortcut.unregister('CommandOrControl+=');
+      // TauriWindow.register('CommandOrControl+M', () => TauriWindow.appWindow.minimize());
+      // TauriWindow.register('CommandOrControl+0', () => set_zoom(default_zoom));
+      // TauriWindow.register('CommandOrControl+-', () => set_zoom(inc_zoom(-1)));
+      // TauriWindow.register('CommandOrControl+=', () => set_zoom(inc_zoom(+1)));
     }
 
     run();
@@ -49,7 +51,7 @@ export default function MyApp({ Component, pageProps }: AppProps) {
     <div data-tauri-drag-region className="h-full w-full">
       <div
         className="data-tauri-drag-region flex h-full w-full origin-center content-center items-center"
-        style={{ transform: `scale(${zoom})` }}
+        // style={{ transform: `scale(${zoom})` }}
       >
         <Component {...pageProps} />
       </div>
