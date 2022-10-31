635effc5099ed97e27c1e2800dac6ac6980c26cf
magus <noah@iamnoah.com>
2022-10-30 17:45:36 -0700

    searchnpcs: remove tauri events

--- src/pages/search-npc/SearchNpc.tsx
+++ src/pages/search-npc/SearchNpc.tsx
@@ -5,18 +5,6 @@ import * as TauriEvents from '@tauri-apps/api/event';
 export function SearchNpc() {
   const [npc, set_npc] = React.useState('');
 
-  React.useEffect(() => {
-    const promise_unlisten = TauriEvents.listen('select', (event) => {
-      console.debug('select', { event });
-    });
-
-    return () => {
-      promise_unlisten.then((unlisten) => {
-        unlisten();
-      });
-    };
-  }, []);
-
   async function greet() {
     // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
     set_npc(await invoke('greet', { name: npc }));
