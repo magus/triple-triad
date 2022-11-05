04199ef3d6bbd7933605b05d4bd476bb704347d5
magus <noah@iamnoah.com>
2022-11-05 16:12:35 -0700

    ui: cleanup state command

--- src-tauri/src/ui/commands/state.rs
+++ src-tauri/src/ui/commands/state.rs
@@ -3,17 +3,10 @@ use tauri::Manager;
 use crate::ui::AppState;
 use crate::ui::AppStateJson;
 
-#[derive(Clone, serde::Serialize)]
-pub struct Payload {
-    state: AppStateJson,
-}
-
 // see https://tauri.app/v1/guides/features/command
 #[tauri::command]
-pub async fn state(app_handle: tauri::AppHandle) -> Result<Payload, String> {
-    let app_handle_state = app_handle.state::<AppState>();
+pub async fn state(app_handle: tauri::AppHandle) -> Result<AppStateJson, String> {
+    let state = app_handle.state::<AppState>();
 
-    Ok(Payload {
-        state: app_handle_state.json(),
-    })
+    Ok(state.json())
 }
