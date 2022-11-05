82b9ca2981cb0736ccdbe47d4c95fce0eaf76de6
magus <noah@iamnoah.com>
2022-11-05 16:13:04 -0700

    ui: AppState type and mock

---
+++ src/core/AppState.ts
@@ -0,0 +1,57 @@
+export type AppState = {
+  status: string;
+  game: Game;
+};
+
+type Game = {
+  board: Board;
+  chaos_card: null;
+  computer: Computer;
+  evaluation_max: number;
+  is_player_first: boolean;
+  last_move: null;
+  player: Player;
+  rules: Rules;
+  score: number;
+  turn: number;
+};
+
+type Card = {
+  id: number;
+  is_empty: boolean;
+  is_guaranteed: boolean;
+  is_player: boolean;
+  modifier: number;
+  name: string;
+  sides: [number, number, number, number];
+  tribe: number;
+};
+
+type Computer = {
+  cards: [Card, Card, Card, Card, Card, Card, Card, Card, Card, Card];
+  cards_used: number;
+};
+
+type Player = {
+  cards: [Card, Card, Card, Card, Card];
+};
+
+type Board = [Card, Card, Card, Card, Card, Card, Card, Card, Card];
+
+type Rules = {
+  all_open: boolean;
+  ascension: boolean;
+  chaos: boolean;
+  descension: boolean;
+  draft: boolean;
+  fallen_ace: boolean;
+  order: boolean;
+  plus: boolean;
+  random: boolean;
+  reverse: boolean;
+  roulette: boolean;
+  same: boolean;
+  sudden_death: boolean;
+  swap: boolean;
+  three_open: boolean;
+};
