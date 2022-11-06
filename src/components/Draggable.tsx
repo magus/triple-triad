7ef61be1a551e13c673c61b35245ebe1522526cc
magus <noah@iamnoah.com>
2022-11-06 13:59:42 -0800

    ui: Draggable persist all styles

--- src/components/Draggable.tsx
+++ src/components/Draggable.tsx
@@ -10,27 +10,29 @@ type Props = {
 };
 
 export function Draggable(props: Props) {
-  const persist_transform = React.useRef(undefined);
+  const persist_style = React.useRef(undefined);
 
   const { isDragging, attributes, listeners, setNodeRef, transform } = useDraggable({
     id: props.id,
     data: props.data,
   });
 
-  const transform_string = CSS.Translate.toString(transform);
-
   const style = {
     zIndex: undefined,
-    transform: transform_string || persist_transform.current,
+    transform: CSS.Translate.toString(transform),
   };
 
   if (isDragging) {
-    // capture ongoing drag so we can persist it when it ends
-    persist_transform.current = transform_string;
-
     // elevate active drag element above other elements
     // normal z-index is 10, multiple by 10 to get 100
     style.zIndex = 100;
+
+    // capture ongoing drag so we can persist it when it ends
+    persist_style.current = style;
+  }
+
+  if (persist_style.current) {
+    Object.assign(style, persist_style.current);
   }
 
   return (
