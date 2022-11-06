94bc29b5a5ea8894b5b81cab746dc407bebb929f
magus <noah@iamnoah.com>
2022-11-06 12:55:37 -0800

    ui: persist drag until next state update

--- src/components/Draggable.tsx
+++ src/components/Draggable.tsx
@@ -10,17 +10,26 @@ type Props = {
 };
 
 export function Draggable(props: Props) {
+  const persist_transform = React.useRef(undefined);
+
   const { isDragging, attributes, listeners, setNodeRef, transform } = useDraggable({
     id: props.id,
     data: props.data,
   });
 
+  const transform_string = CSS.Translate.toString(transform);
+
   const style = {
     zIndex: undefined,
-    transform: CSS.Translate.toString(transform),
+    transform: transform_string || persist_transform.current,
   };
 
   if (isDragging) {
+    // capture ongoing drag so we can persist it when it ends
+    persist_transform.current = transform_string;
+
+    // elevate active drag element above other elements
+    // normal z-index is 10, multiple by 10 to get 100
     style.zIndex = 100;
   }
 
