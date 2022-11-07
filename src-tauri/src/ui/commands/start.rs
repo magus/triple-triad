23b0f049a9c46d7d6ecacebda60571902bb1e04a
magus <noah@iamnoah.com>
2022-11-06 17:51:45 -0800

    ui: start_internal for calling from other commands

--- src-tauri/src/ui/commands/start.rs
+++ src-tauri/src/ui/commands/start.rs
@@ -6,6 +6,10 @@ use crate::ui::AppStateJson;
 // see https://tauri.app/v1/guides/features/command
 #[tauri::command]
 pub async fn start(app_handle: tauri::AppHandle) -> Result<AppStateJson, String> {
+    return start_internal(&app_handle);
+}
+
+pub fn start_internal(app_handle: &tauri::AppHandle) -> Result<AppStateJson, String> {
     let state = app_handle.state::<AppState>();
 
     // grab game via mutex and clone for mutating and reassigning back to mutex
